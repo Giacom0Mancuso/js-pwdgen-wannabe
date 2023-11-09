1665b6946c3877611c776cf7f15a3145c8dfdc27
Giacom0Mancuso <giacomomancuso35@email.com>
2023-11-09 13:00:58 +0100

    base done

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let name = prompt("inserisci il tuo nome");
+let surname = prompt("inserisci il tuo cognome");
+let favColor = prompt("inserisci il tuo colore preferito");
+
+let password = name + surname + favColor + 21;
+console.log(password);
